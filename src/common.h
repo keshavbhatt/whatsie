cc2852a77c4af51b1ef5291149eb5740373da818
Keshav Bhatt <keshavnrj@gmail.com>
2024-05-25 21:20:44 +0530

    chore: update UA to latest Chrome

--- src/common.h
+++ src/common.h
@@ -3,8 +3,8 @@
 #include <QString>
 
 // userAgent
-//QString defaultUserAgentStr = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
-QString defaultUserAgentStr = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+// QString defaultUserAgentStr = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+QString defaultUserAgentStr = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36";
 
 //appAutoLock
 int defaultAppAutoLockDuration = 30;
